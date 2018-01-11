fc79ceb63dd84fd0521ed71c50253b491008b20a
Kenneth Yu <kenyu@ucdavis.edu>
2018-01-10 23:15:46 -0800

    handle Simple String success case and \n not allowed

--- resp/resp.rb
+++ resp/resp.rb
@@ -1,7 +1,15 @@
 class RESP
 
   def self.encode(value)
-    return '+OK\r\n'
+
+    if value.class == String
+
+      if value.include?('\n')
+        return '-ERR illegal character \n'
+      end
+      "+#{value}\\r\\n"
+    end
+
   end
 
 end
--- resp/spec/resp_spec.rb
+++ resp/spec/resp_spec.rb
@@ -2,10 +2,26 @@ require 'spec_helper'
 
 describe "RESP" do
 
-  context "encode" do
+  describe "#encode" do
 
-    it "should" do
-      expect(RESP.encode("OK")).to eql('+OK\r\n')
+    context "Simple Strings" do
+
+      context "success" do
+
+        it "should have a prefix of '+' and suffix of '\\r\\n'" do
+          expect(RESP.encode("OK")).to eql('+OK\r\n')
+        end
+
+      end
+
+      context "errors" do
+
+        it 'should not allow "\n"' do
+          expect(RESP.encode('Zasdasasf\n')).to eql('-ERR illegal character \n')
+        end
+
+      end
+
     end
 
   end
